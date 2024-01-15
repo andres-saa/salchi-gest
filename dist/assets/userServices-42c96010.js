c8b1e645cfc24e5567cc90605229e0a96fbe12b8
andres-saa <andrew19f@gmail.com>
2024-01-15 16:25:56 +0000

    nuevos noticias

--- dist/assets/userServices-42c96010.js
+++
@@ -1,1 +0,0 @@
-import{U as e}from"./index-6dc4c913.js";const s=async()=>{try{const r=await fetch(`${e}/employers`,{method:"GET"});return r.ok?await r.json():(console.error("Error al obtener usuarios:",r.statusText),null)}catch(r){return console.error("Error al obtener usuarios:",r),null}};export{s as g};
--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -3,29 +3,31 @@
         style="max-width: 900px;display: ;height: 70vh;align-items: center;justify-content: center; flex-direction: column; ">
         <p class="text-2xl lg:text-4xl   col-12 text-center" style="font-weight: bold;width: auto; "> Novedades</p>
 
-        <a href="https://forms.gle/mr7CD1sWT6C2NRKi8">
-        <Button class="  "
-                            style="display: flex;justify-content: center;width: 100%; align-items: center; color: rgba(0, 0, 0, 0.808);border: none; font-weight: bold; height: 20vh;min-height:200px; background-color: rgb(255, 211, 211);">
+        <a class="col-12 p-2" href="https://forms.gle/mr7CD1sWT6C2NRKi8">
 
-                            <span class="text-3xl  p-3 col-12" style="height: auto; position: absolute; z-index: 100;color: rgb(255, 255, 255); text-shadow: 0 0 5px black;">
+            <div class="col-12 p-4" style=""> 
+                <Button class="p-0  " 
+                            style="box-shadow: 0 0 20px rgba(0, 0, 0, 0.524); display: flex;justify-content: center;width: 100%; align-items: center; color: rgba(0, 0, 0, 0.808);border: none; font-weight: bold; height: ;min-height:200px; background-color: rgb(255, 255, 255);">
+
+                            <!-- <span class="text-3xl  p-3 col-12" style="height: auto; position: absolute; z-index: 100;color: rgb(255, 255, 255); text-shadow: 0 0 5px black;">
                                 Encuesta clima organizacional 
-                            </span>
+                            </span> -->
                     
 
-                            <div style="width: 100%;height: 100%; object-fit: ; position: absolute;" class="imagen p-0">
-                                <img class=" p-0"  alt="" src="/images/meet.jpg" style="height: 100%;width: 100%; object-fit: cover;">
+                            <div style=" position: ;" class=" p-0">
+                                <img class=" p-0"  alt="" src="/images/clima.jpg" style="height: 100%;width: 100%; object-fit: contain;">
 
                             </div>
 
-                            <div class="pl-3 text-2xl" style= "display: flex; align-items: center; width: 15rem; height: 3rem;left: 0; top: 0; background-color: red;color: white; position: absolute;z-index: 100;border-radius: 0 0  10rem 0;
+                            <div class="pl-6 text-2xl" style= "display: flex; align-items: center; width: 10rem; height: 3rem;right: 0; top: 0; background-color: red;color: white; position: absolute;z-index: 100;border-radius: 0 0   0 10rem;
                            ">
                                 Nuevo
                             </div>
 
                
-                            <div class="pl-3 text-2xl" style= "display: flex; align-items: center; width: 15rem; height: 3rem;right: 0; bottom: 0; background-color: rgb(255, 255, 255);outline: 10px solid rgb(255, 255, 255); color: white; position: absolute;z-index: 100;border-radius: 10rem 0 0   0;
+                            <div class="pl-3 text-2xl" style= "display: flex; align-items: center;justify-content: center; width: 10rem; height: 2rem;right: 0; bottom: 0; background-color: rgb(255, 255, 255);outline: 10px solid rgb(255, 255, 255); color: white; position: absolute;z-index: 100;border-radius: 10rem 0 0   0;
                            ">
-                                <img style="width: 80%; object-fit: contain;" class="p-0" src="/images/forms.png" alt="">
+                                <img style="width: 100%; object-fit: contain;height: 100%;" class="p-0" src="/images/forms.png" alt="">
                             </div>
                         
                         
@@ -34,6 +36,15 @@
                         
                         
                         </Button>
+            </div>
+       
+
+
+
+
+
+
+                        
                     </a>
 
                         <p class="text-2xl lg:text-4xl   col-12 text-center" style="font-weight: bold;width: auto; "> Accesos rápidos</p>
@@ -58,7 +69,7 @@
                     
 
                             <div style="width: 100%;height: 100%; object-fit: ; position: absolute;" class="imagen p-0">
-                                <img class=" p-0" :src="menu.imagen" alt="" style="height: 100%;width: 100%; object-fit: cover;">
+                                <img class="imagen p-0" :src="menu.imagen" alt="" style="height: 100%;width: 100%; object-fit: cover;">
 
                             </div>
                         
