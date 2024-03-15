244458377e3830ad0f814b7cf5f949059ab92288
andres-saa <andrew19f@gmail.com>
2024-03-15 16:21:54 -0500

    nueva interfaz

--- dist/assets/error-f362ab7d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-224de96d.js";const a={};function e(n,o){return" por aqui no hay nada para ti "}const c=r(a,[["render",e]]);export{c as default};
---
+++ dist/assets/siteService-407aa805.js
@@ -0,0 +1,1 @@
+import{U as t}from"./index-7972d487.js";const r={async getSites(){try{return(await fetch(`${t}/sites`)).json()}catch(s){console.log(s)}}};export{r as s};
