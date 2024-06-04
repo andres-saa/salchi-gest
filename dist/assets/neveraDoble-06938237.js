8db7fa01239e110935593e138aa4717fad337ca6
andres-saa <andrew19f@gmail.com>
2024-06-04 08:51:58 -0500

    correccion del horario

--- dist/assets/neveraDoble-06938237.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-a303b041.js";const t={},s={class:"col-12",style:{"max-width":"1024px",margin:"auto"}};function a(n,r){return c(),o("div",s)}const l=e(t,[["render",a]]);export{l as default};
---
+++ dist/assets/pathService-f0b31280.js
@@ -0,0 +1,1 @@
+import{k as e}from"./index-8962327d.js";const s={isActiveRoute(t){return e().path.includes(t)}};export{s as P};
