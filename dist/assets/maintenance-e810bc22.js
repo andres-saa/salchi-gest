57d596f9919381a6ac560d5a13f412e2d58e027b
andres-saa <andrew19f@gmail.com>
2024-08-11 12:39:51 -0500

    nuevos

--- dist/assets/maintenance-e810bc22.js
+++
@@ -1,1 +0,0 @@
-import{G as e}from"./index-bca453a0.js";const n=e("maintance",{persist:{key:"maintance",storage:localStorage,paths:["currentSite"]},state:()=>({currentSite:{}}),getters:{},actions:{setCurrentSite(t){this.currentSite=t}}});export{n as m};
--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        // ...datos del manifest
-      }
-    })
+    
   ],
   resolve: {
     alias: {
