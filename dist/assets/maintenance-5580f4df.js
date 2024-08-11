0a79c1104b711c3f48385e261c92c531f10c3326
andres-saa <andrew19f@gmail.com>
2024-08-11 11:04:09 -0500

    salchi new

--- dist/assets/maintenance-5580f4df.js
+++
@@ -1,1 +0,0 @@
-import{G as e}from"./index-66648269.js";const n=e("maintance",{persist:{key:"maintance",storage:localStorage,paths:["currentSite"]},state:()=>({currentSite:{}}),getters:{},actions:{setCurrentSite(t){this.currentSite=t}}});export{n as m};
